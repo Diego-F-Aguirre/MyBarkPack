98dff821bf5dc8503314d16be815b7d48e1f074f
Diego-F-Aguirre <diego3884@gmail.com>
2016-05-10 16:19:09 -0600

    Fixed issues of headers being deleted
    Updated SectionHeader to be a UITableViewHeaderFooterView instead of a UITableVewCell

--- MyBarkPack/SectionHeaderView.swift
+++ MyBarkPack/SectionHeaderView.swift
@@ -1,40 +1,26 @@
 //
-//  SectionHeaderTableViewCell.swift
+//  SectionHeaderView.swift
 //  MyBarkPack
 //
-//  Created by Diego Aguirre on 5/3/16.
+//  Created by Diego Aguirre on 5/10/16.
 //  Copyright © 2016 home. All rights reserved.
 //
 
 import UIKit
 
-protocol SectionHeaderTableViewCellDelegate {
-    func didSelectUserHeaderTableViewCell(sectionHeader: SectionHeaderTableViewCell, selected: Bool, type: Type)
-}
+class SectionHeaderView: UITableViewHeaderFooterView {
 
-class SectionHeaderTableViewCell: UITableViewCell {
+    
     @IBOutlet weak var labelContainerView: LabelContainerView!
     @IBOutlet weak var sectionTitleLabel: UILabel!
     @IBOutlet weak var plusButton: UIButton!
     
     var type: Type?
     
-    var delegate: SectionHeaderTableViewCellDelegate?
+    weak var delegate: SectionHeaderViewDelegate?
     var dog: Dog?
     
     let sections = [Type.Meals, Type.Exercise, Type.Health, Type.Training, Type.Misc]
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-}
-
-extension SectionHeaderTableViewCell {
-    @IBAction func plusButtonPressed(sender: AnyObject) {
-        if let type = type {
-            delegate?.didSelectUserHeaderTableViewCell(self, selected: plusButton.selected, type: type )
-        }
-    }
     
     func updateDogWithGender(dog: Dog?) {
         guard let dog = dog else { return }
@@ -45,5 +31,15 @@ extension SectionHeaderTableViewCell {
             labelContainerView.backgroundColor = UIColor.lightPink()
         }
     }
+    
+    @IBAction func plusButtonPressed(sender: AnyObject) {
+        if let type = type {
+            delegate?.didSelectUserHeaderView(self, selected: plusButton.selected, type: type )
+        }
+    }
+    
 }
 
+protocol SectionHeaderViewDelegate: class {
+    func didSelectUserHeaderView(sectionHeader: SectionHeaderView, selected: Bool, type: Type)
+}
--- MyBarkPack/TaskTableViewCell.swift
+++ MyBarkPack/TaskTableViewCell.swift
@@ -18,7 +18,6 @@ class TaskTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 }
 
